69df2f32fe1a8e2b11a9a7833a1c540f4afc3786
Sergio <sergio@MacBook-Pro.local>
2024-07-30 12:43:50 -0700

    fix: test v0.0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 export default async function Home() {
-  const api = process.env.API_LOCAL;
-  const response = await fetch(`${api}/api`);
-  const { products } = await response.json();
-  console.log("Products: ", products);
+  const api = process.env.NEXT_PUBLIC_API_LOCAL;
+  const api2 = process.env.API_LOCAL;
+  console.log("api: ", api);
+  console.log("api2: ", api2);
+
+  // const response = await fetch(`${api}/api`);
+  // const { products } = await response.json();
+  // console.log("Products: ", products);
 
   return (
     <div>
-      <ul>
+      <h1>Hola</h1>
+      {/* <ul>
         {products.map((product: any) => (
           <li key={product.id}>{product.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
