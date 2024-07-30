8672f2cb9445727675ac1ef3859760ad3c5081da
Sergio <sergio@MacBook-Pro.local>
2024-07-30 12:51:26 -0700

    fix: test v0.0.5

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   env: {
     API_LOCAL: process.env.NEXT_PUBLIC_API_LOCAL,
   },
 };
 
 export default nextConfig;
+// NEXT_PUBLIC_API_LOCAL
+// fake-api-demo.vercel.app
+// fake-api-demo.vercel.app
+// https://fake-api-demo-git-main-chespisans-projects.vercel.app/
+// https://fake-api-demo-phlp2vst1-chespisans-projects.vercel.app/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
 export default async function Home() {
   const api = process.env.NEXT_PUBLIC_API_LOCAL;
-  const api2 = process.env.API_LOCAL;
+  // const api2 = process.env.API_LOCAL;
   console.log("api: ", api);
-  console.log("api2: ", api2);
-
-  // const response = await fetch(`${api}/api`);
-  // const { products } = await response.json();
-  // console.log("Products: ", products);
+  // console.log("api2: ", api2);
+  let productsFake = [];
+  try {
+    const response = await fetch(`${api}/api`);
+    const { products } = await response.json();
+    console.log("Products: ", products);
+    productsFake = products;
+  } catch (error) {
+    console.log("error: ", error);
+  }
 
   return (
     <div>
-      <h1>Hola</h1>
-      {/* <ul>
-        {products.map((product: any) => (
-          <li key={product.id}>{product.name}</li>
+      <ul>
+        {productsFake?.map((product: any) => (
+          <li key={product?.id}>{product?.name}</li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 }
